ffd1e220f443e42a358720c06f51fbea345ddd9d
rodhis <rodrigonoliva@gmail.com>
2025-01-30 17:10:52 -0300

    added components folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,11 @@
 export default function HomePage() {
-
+    return (
+        <>
+            <Hero />
+            <FeaturedPosts />
+        </>
+    )
 }
+
+//1 - Hero Page - Apresentação da página e seus criadores
+// 2 - Posts em destaque
