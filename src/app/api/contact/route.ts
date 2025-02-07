e3f6a701b3fc87f9190b387d6cd24a9d26e586ff
rodhis <rodrigonoliva@gmail.com>
2025-02-07 19:24:42 -0300

    finished API data for posting contacts via mongodb

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,13 @@
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { NextResponse } from 'next/server'
 
-export async function POST(request: Request) {
+export async function POST(req: Request) {
     try {
-        const body = await request.json()
-        const { email, name, message } = body
+        const body = await req.json()
+        const { email, name, message, id } = body
 
         if (
             !email ||
@@ -16,8 +20,33 @@ export async function POST(request: Request) {
             return NextResponse.json({ message: 'Invalid input.' }, { status: 422 })
         }
 
-        const newMessage = { email, name, message }
-        console.log(newMessage)
+        const newMessage = { email, name, message, id }
+
+        const username = process.env.USERNAME
+        const password = process.env.PASSWORD
+
+        let client
+
+        try {
+            client = await MongoClient.connect(
+                `mongodb+srv://${username}:${password}@next1.vsk9a5j.mongodb.net/`
+            )
+        } catch (error) {
+            NextResponse.json({ message: `Could not connect to database: ${error}` }, { status: 500 })
+            return
+        }
+        const db = client.db('next1')
+
+        try {
+            const result = await db.collection('next-blog-messages').insertOne(newMessage)
+            newMessage.id = result.insertedId
+        } catch (error) {
+            client.close()
+            NextResponse.json({ message: `Storing message failed: ${error}` }, { status: 500 })
+            return
+        }
+
+        client.close()
 
         return NextResponse.json({ message: 'Success!' }, { status: 201 })
     } catch (error) {
