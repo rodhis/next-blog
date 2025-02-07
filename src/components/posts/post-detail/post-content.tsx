bc5c3e80451c2138fc2bb8be4ab835af79b7afb6
rodhis <rodrigonoliva@gmail.com>
2025-02-07 17:44:23 -0300

    fixed react markdown custom renderers paragraph class

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import {  ImgHTMLAttributes } from 'react'
 
 import ReactMarkdown from 'react-markdown'
 
@@ -8,21 +7,26 @@ import { Post } from '@/interfaces/Post'
 
 import styles from '@/styles/post-content.module.css'
 
-
-
-
 export default function PostContent({ post }: { post: Post }) {
     const imagePath = `/images/posts/${post.slug}/${post.image}`
 
     const customRenderers = {
-        img: (props: ImgHTMLAttributes<HTMLImageElement>) => {
-            const { src, alt } = props
-            const imageSrc = src?.startsWith('/') ? src : `/images/posts/${post.slug}/${src}`
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        p(paragraph: any) {
+            const { node } = paragraph
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0]
 
-            return <Image src={imageSrc} alt={alt || ''} width={600} height={300} />
+                const imageSrc = image.properties.src.startsWith('/') ? image.properties.src : `/images/posts/${post.slug}/${image.properties.src}`
+                return (
+                    <div className={styles.image}>
+                        <Image src={imageSrc} alt={image.alt || ''} width={600} height={300} />
+                    </div>
+                )
+            }
+
+            return <p>{paragraph.children}</p>
         },
-      
-       
     }
 
     return (
