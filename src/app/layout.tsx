506de55510e0b62742a38d92822d58b85f0df85b
rodhis <rodrigonoliva@gmail.com>
2025-02-25 18:47:43 -0300

    implemented next-auth session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
+
 import '@/styles/globals.css'
 import MainNavigation from '@/components/main-navigation/main-navigation'
+import Providers from '@/components/providers/session-provider'
 
 export const metadata = {
-    name: 'viewport',
-    content: 'width=device-width, initial-scale=1',
     title: 'Next Level Blog',
     description: "Rodhis' Tech Blog",
+    viewport: { 
+        width: 'device-width',
+        initialScale: 1,
+      },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
+                <Providers>
                 <MainNavigation />
                 <main>
                     {children}
                     <div id="notifications"></div>
                 </main>
+                </Providers>
             </body>
         </html>
     )
--- src/components/main-navigation/main-navigation.tsx
+++ src/components/main-navigation/main-navigation.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 import styles from '@/styles/main-navigation.module.css'
 import Logo from './logo'
 
 export default function MainNavigation() {
+    const { data: session, status } = useSession()
+
+    console.log(session)
+    console.log(status)
+
     return (
         <header className={styles.header}>
             <Link href="/">
@@ -17,9 +25,16 @@ export default function MainNavigation() {
                     <li>
                         <Link href="/contact">Contact</Link>
                     </li>
-                    <li>
-                        <Link href="/profile">Profile</Link>
-                    </li>
+                    {!session && status !== 'loading' && (
+                        <li>
+                            <Link href="/auth">Login</Link>
+                        </li>
+                    )}
+                    {session && (
+                        <li>
+                            <Link href="/profile">Profile</Link>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
