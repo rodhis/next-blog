5891439d079a766b065889f148f9c91382a7c599
rodhis <rodrigonoliva@gmail.com>
2025-02-26 18:32:53 -0300

    implemented page guard on profile page

--- src/components/admin-page/admin-profile.tsx
+++ src/components/admin-page/admin-profile.tsx
@@ -1,15 +1,39 @@
-import AdminForm from './admin-form';
+'use client'
 
-import styles from '@/styles/admin-profile.module.css';
+import { getSession } from 'next-auth/react'
+import { Session } from 'next-auth'
+
+import { useState, useEffect } from 'react'
+
+import AdminForm from './admin-form'
+
+import styles from '@/styles/admin-profile.module.css'
 
 export default function AdminProfile() {
-  // Redirect away if NOT auth
+    const [isLoading, setIsLoading] = useState(true)
+    const [loadedSession, setLoadedSession] = useState<Session | null>(null)
 
-  return (
-    <section className={styles.profile}>
-      <h1>Your User Profile</h1>
-      <AdminForm />
-    </section>
-  );
+    useEffect(() => {
+        getSession().then((session) => {
+            if (!session) {
+                window.location.href = '/auth'
+            } else {
+                setIsLoading(false)
+                setLoadedSession(session)
+            }
+        })
+    }, [])
+
+    if (isLoading) {
+        return <p className={styles.profile}>Loading...</p>
+    }
+
+    return (
+        loadedSession && (
+            <section className={styles.profile}>
+                <h1>Your User Profile</h1>
+                <AdminForm />
+            </section>
+        )
+    )
 }
-
