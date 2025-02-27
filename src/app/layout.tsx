a7a5016482a2531f094b0ee4c42063b16b08a928
rodhis <rodrigonoliva@gmail.com>
2025-02-27 13:30:42 -0300

    added a modification on session provider to prevent redundant session requests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,28 @@
+import { getServerSession } from 'next-auth'
 
-import '@/styles/globals.css'
 import MainNavigation from '@/components/main-navigation/main-navigation'
 import Providers from '@/components/providers/session-provider'
+import { authOptions } from '@/lib/auth'
+
+import '@/styles/globals.css'
 
 export const metadata = {
     title: 'Next Level Blog',
     description: "Rodhis' Tech Blog",
-
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions)
+
     return (
         <html lang="en">
             <body>
-                <Providers>
-                <MainNavigation />
-                <main>
-                    {children}
-                    <div id="notifications"></div>
-                </main>
+                <Providers session={session}>
+                    <MainNavigation />
+                    <main>
+                        {children}
+                        <div id="notifications"></div>
+                    </main>
                 </Providers>
             </body>
         </html>
