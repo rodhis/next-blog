e66a3ca320d3ddbdabf8f3221190d33f67f08740
rodhis <rodrigonoliva@gmail.com>
2025-03-13 17:47:35 -0300

    adjusted admin-profile component to use notification context

--- src/components/admin-page/admin-profile.tsx
+++ src/components/admin-page/admin-profile.tsx
@@ -1,46 +1,32 @@
 'use client'
 
-import { useState } from 'react'
-
 import AdminForm from './admin-form'
 
 import styles from '@/styles/admin-profile.module.css'
 
 export default function AdminProfile() {
-    const [message, setMessage] = useState<string | null>(null)
     const changePasswordHandler = async (passwordData: { oldPassword: string; newPassword: string }) => {
-        try {
-            const response = await fetch('/api/change-password', {
-                method: 'PATCH',
-                body: JSON.stringify(passwordData),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
+        const response = await fetch('/api/change-password', {
+            method: 'PATCH',
+            body: JSON.stringify(passwordData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
 
-            const data = await response.json()
+        const data = await response.json()
 
-            if (!response.ok) {
-                throw new Error(data.error || 'Failed to change password')
-            }
-
-            setMessage('Password changed successfully!')
-            console.log('Success:', data)
-        } catch (error) {
-            console.error('Error:', error)
-            if (error instanceof Error) {
-                setMessage(error.message || 'Failed to change password')
-            } else {
-                setMessage('Failed to change password')
-            }
+        if (!response.ok) {
+            throw new Error(data.error || 'Failed to change password')
         }
+
+        console.log('Success:', data)
     }
 
     return (
         <section className={styles.profile}>
             <h1>Your User Profile</h1>
             <AdminForm onChangePassword={changePasswordHandler} />
-            {message && <div className={styles.message}>{message}</div>}
         </section>
     )
 }
