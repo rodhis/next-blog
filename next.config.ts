9c9a3dfb21a62403f89ba77028dc6b35b3dc9f2a
Rodhis <rodrigonoliva@gmail.com>
2025-03-27 20:29:25 -0300

    improved the hero texts descriptions and added stacks icons

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    images: {
+        domains: ['raw.githubusercontent.com', 'github.com'],
+    },
 }
 
 export default nextConfig
--- src/components/home-page/hero.tsx
+++ src/components/home-page/hero.tsx
@@ -9,8 +9,17 @@ export default function Hero() {
             <div className={styles.image}>
                 <Image src={myPic} alt="An image of Rodhis" width={300} height={300} />
             </div>
-            <h1>Hello! I&apos;m Rodrigo, but you can call me Rodhis!</h1>
-            <p>I&apos;m a full stack developer, and I&apos;m making this blog to tell you bits about my work.</p>
+            <h1>Hello There! I&apos;m Rodrigo!</h1>
+            <p>I&apos;m a full stack developer, and here you&apos;ll see posts about the technologies I use while also being able to browse previews and videos of some of my personal projects.</p>
+            <h1>My Stacks:</h1>
+            <div className={styles.icons}>
+            <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg" alt="ts icon" width={40} height={40} />
+            <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" alt="react icon" width={40} height={40} />
+            <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg" alt="next icon" width={40} height={40} />
+            <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg" alt="tailwindcss icon" width={40} height={40} />
+            <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" alt="node icon" width={40} height={40} />
+            <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg" alt="express icon" width={40} height={40} />
+            </div>
         </section>
     )
 }
