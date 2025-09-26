4686592e37e4c4c75e1a654ad0f1fa8374733333
rodhis <rodrigonoliva@gmail.com>
2025-09-26 18:09:17 -0300

    post image size calculation is now dynamic for icons and bigger images

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -23,9 +23,17 @@ export default function PostContent({ post }: { post: Post }) {
                 const imageSrc = image.properties.src.startsWith('/')
                     ? image.properties.src
                     : `/images/posts/${post.slug}/${image.properties.src}`
+                    
+                const altText = image.properties.alt || ''
+                const isSmall = ['icon', 'logo', 'badge'].some(keyword => 
+    altText.toLowerCase().includes(keyword)
+)
+                const width = isSmall ? 180 : 700
+                const height = isSmall ? 150 : 400
+
                 return (
                     <div className={styles.image}>
-                        <Image src={imageSrc} alt={image.alt || ''} width={600} height={300} />
+                        <Image src={imageSrc} alt={image.alt || ''} width={width} height={height} />
                     </div>
                 )
             }
